76e949691c3b227a15bc6a4218aa1c99ce414ca4
Danijel Malinovic <danijelmalinovic2@gmail.com>
2026-07-04 09:28:13 +0200

    anpassungen

--- Zbw.PF2.ContactManager/Forms/Start.cs
+++ Zbw.PF2.ContactManager/Forms/Start.cs
@@ -1,20 +1,10 @@
-using MaterialSkin.Controls;
-
 namespace Zbw.PF2.ContactManager
 {
-    public partial class Start : MaterialForm
+    public partial class Start : Form
     {
         public Start()
         {
             InitializeComponent();
-            var materialSkinManager = MaterialSkin.MaterialSkinManager.Instance;
-            materialSkinManager.AddFormToManage(this);
-            materialSkinManager.Theme = MaterialSkin.MaterialSkinManager.Themes.LIGHT;
-            materialSkinManager.ColorScheme = new MaterialSkin.ColorScheme(
-                MaterialSkin.Primary.BlueGrey800, MaterialSkin.Primary.BlueGrey900,
-                MaterialSkin.Primary.BlueGrey500, MaterialSkin.Accent.Pink200,
-                MaterialSkin.TextShade.WHITE
-            );
         }
     }
 }
---
+++ Zbw.PF2.ContactManager/UI/MainForm.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Zbw.PF2.ContactManager.UI;
+
+public partial class MainForm : Form
+{
+    public MainForm()
+    {
+        InitializeComponent();
+    }
+}
