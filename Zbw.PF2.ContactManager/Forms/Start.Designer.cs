f8a4e623b405a7ea39e1e85cc90ac445aee85d3c
hermanoarbunita <arbutinanikola0@gmail.com>
2026-07-04 10:33:23 +0200

    feat: delete Comment

--- Zbw.PF2.ContactManager/Forms/Start.Designer.cs
+++ Zbw.PF2.ContactManager/Forms/Start.Designer.cs
@@ -18,6 +18,8 @@
                 components.Dispose();
             }
             base.Dispose(disposing);
+
+            //Kommentar2
         }
 
         #region Windows Form Designer generated code
