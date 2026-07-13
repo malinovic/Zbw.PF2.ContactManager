da455841e2b3d0cf95c06f77949f87ae5da3b107
hermanoarbunita <arbutinanikola0@gmail.com>
2026-07-13 18:14:21 +0200

    Das ist ein Test-Commit um zu sehen ob der Workflow für GitHub von mir verstanden wurde

---
+++ Zbw.PF2.ContactManager/test_newBranch.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Zbw.PF2.ContactManager;
+
+internal class test_newBranch
+{
+    public string Name { get; set; }
+}
