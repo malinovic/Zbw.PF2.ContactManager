4d8c07b9e9606cdf307997566d6a353e987e58d1
hermanoarbunita <arbutinanikola0@gmail.com>
2026-07-15 14:21:19 +0200

    feat: setup datastructure with Models and Enums

---
+++ Zbw.PF2.ContactManager/Models/Employee.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Zbw.PF2.ContactManager.Models;
+
+internal class Employee : Person
+{
+}
---
+++ Zbw.PF2.ContactManager/Models/Enums/Enums.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Zbw.PF2.ContactManager.Models.Enums;
+
+internal class Enums
+{
+    // Enums for class Person
+    public enum Salutation { Herr, Frau,};
+    public enum Sex {Mann, Frau };
+
+
+    //Enums for class Employee
+    public enum EmployeeStatus { aktiv, passiv}
+    public enum EmployeeSeniorLevel {Level0 = 0, Level1 = 1, Level2 = 2, Level3 = 3, Level4 = 4, Level5 = 5 };
+
+    //Enums for class Customer
+    public enum CustomerStatus { aktiv, passiv};
+
+    
+}
