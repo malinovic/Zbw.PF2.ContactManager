eb2034c7132eb4928f222c562ee6090ada67023d
hermanoarbunita <arbutinanikola0@gmail.com>
2026-07-19 14:54:18 +0200

    feat: setup datastructure (#34)
    Co-authored-by: Danijel Malinovic <danijelmalinovic2@gmail.com>

---
+++ Zbw.PF2.ContactManager/Models/Customer.cs
@@ -0,0 +1,16 @@
+﻿
+
+using Zbw.PF2.ContactManager.Core.Constants;
+
+
+namespace Zbw.PF2.ContactManager.Models;
+
+public class Customer : Person
+{
+
+    public string CustomerNumber { get; set; }
+    public string CustomerCompanyName { get; set; }
+    public Status CustomerStatus { get; set; }
+    public CustomerType CustomerType { get; set; }
+
+}
