544a0f30ca78d0af8ca48a384277effd8291bb82
hermanoarbunita <arbutinanikola0@gmail.com>
2026-07-31 16:06:13 +0200

    add structure and classes for validation employee

---
+++ Zbw.PF2.ContactManager/Validation/ValidationEmployee/EmployeeInput.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using Zbw.PF2.ContactManager.Core.Constants;
+
+namespace Zbw.PF2.ContactManager.Validation.ValidationEmployee;
+
+public sealed class EmployeeInput
+{
+    public Salutation? Salutation { get; init; }
+    public string FirstName { get; init; } = string.Empty;
+    public string LastName {  get; init; } = string.Empty;
+    public DateTime Birthday {  get; init; } = DateTime.Today;
+    public Sex? Sex { get; init; }
+    public string Title {  get; init; } = string.Empty;
+
+    public string PhoneNumberCompany { get; init; } = string.Empty;
+    public string PhoneNumberMobile { get; init; } = string.Empty;
+    public string Email { get; init; } = string.Empty;
+
+    public string PrivateStreetName { get; init; } = string.Empty;
+    public string PrivateStreetNumber { get; init; } = string.Empty;
+    public string PrivateZipCode { get; init; } = string.Empty;
+    public string PrivateCity { get; init; } = string.Empty;
+
+    public string EmployeeNumber { get; init; } = string.Empty;
+    public string Department { get; init; } = string.Empty;
+    public string AhvNumber { get; init; } = string.Empty;
+    public string Nationality { get; init; } = string.Empty;
+    public string EmployeeStatusMessage { get; init; } = string.Empty;
+    public int EmploymentRate { get; init; }
+    public string Role { get; init; } = string.Empty;
+    public int ApprenticeshipYears { get; init; }
+    public Status? EmployeeStatus { get; init; }
+    public EmployeeSeniorLevel? SeniorLevel { get; init; }
+    public DateTime DateOfHire { get; init; } = DateTime.Today;
+
+    public string WorkStreetName { get; init; } = string.Empty;
+    public string WorkStreetNumber { get; init; } = string.Empty;
+    public string WorkZipCode { get; init; } = string.Empty;
+    public string WorkCity { get; init; } = string.Empty;
+
+}
